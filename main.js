e6f922b8c28d20c23d758f2490aad8adf473df9b
Oscar Andersson <anderssonoscar0@gmail.com>
2019-03-24 23:41:56 +0100

    Add error message when trying to move people into moveer

--- main.js
+++ main.js
@@ -15,51 +15,48 @@ client.on('message', message => {
   const args = message.content.slice(config.discordPrefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   if (command === 'move') {
-    if (args < 1) {
-      message.channel.send('I think you forgot to @mention someone?');
-      console.log(new Date().toLocaleTimeString() + ' - ' + message.guild.name + ' - @Mention is missing');
-    }
-    // The voice channel ID the author of the message sits in
-    const userVoiceRoomID = message.member.voiceChannelID;
-    // The authors ID
-    const authorID = message.author.id;
-    // Which server the message comes from
-    const guild = message.guild;
+    const userVoiceRoomID = message.member.voiceChannelID; // ID of the authors voice room
+    const authorID = message.author.id; // The author ID
+    const guild = message.guild; // The guild where the user sends its message
+    const messageMentions = message.mentions.users.array(); // Mentions in the message
+    const guildChannels = guild.channels.find(channel => channel.name === 'Moveer'); // Search for the voiceroom Moveer
+    const usersInMoveeer = guildChannels.members; // The members ofthe Moveer voice room
 
-    // Mentions in the message
-    const messageMentions = message.mentions.users.array();
-    const guildChannels = guild.channels.find(channel => channel.name === 'Moveer');
+    // Check for errors in the message
+    // Make sure there's a voice room called Moveer
     if (guildChannels === null || guildChannels.members == undefined) {
-      // There's no voice channel named "Moveer"
       console.log(new Date().toLocaleTimeString() + ' - ' + message.guild.name + ' - No voice channel called Moveer');
       message.channel.send('Theres no voice channel named Moveer');
-    } else {
-      const usersInMoveeer = guildChannels.members;
-      // What to send in the discord channel
-      for (var i = 0; i < messageMentions.length; i++) {
-        if (usersInMoveeer.has(messageMentions[i].id)) {
-          if(messageMentions[i].id === authorID) {
-            message.channel.send("You can't move yourself.. :) " + messageMentions[i]);
-          } else {
-            console.log(new Date().toLocaleTimeString() + ' - ' + message.guild.name + ' - Moving a user');
-          message.channel.send(
-            'Moving: ' +
-              messageMentions[i] +
-              '. By request of ' +
-              '<@' +
-              authorID +
-              '>'
-          );
-          guild.member(messageMentions[i].id).setVoiceChannel(userVoiceRoomID);
-          }
-        } else {
-          console.log(new Date().toLocaleTimeString() + ' - ' + message.guild.name + ' - Not moving a user. User in wrong channel.');
-          message.channel.send(
-            'Not moving: ' +
-              messageMentions[i].username +
-              '. Is the user in the correct voice channel?'
-          );
+      return;
+    }
+    // Make sure the user @mentions someone
+    if (args < 1) {
+      message.channel.send('I think you forgot to @mention someone?' + '<@' + authorID + '>');
+      console.log(new Date().toLocaleTimeString() + ' - ' + message.guild.name + ' - @Mention is missing ');
+      return;
+    }
+    // Stop people from trying to move people into Moveer
+    if (usersInMoveeer.has(authorID)){
+      message.channel.send("You can't move people into this voice room " + '<@' + authorID + '>');
+      console.log(new Date().toLocaleTimeString() + ' - ' + message.guild.name + ' - User trying to move people into Moveer');
+      return;
+    }
+
+    // No errors in the message, try moving everyone in the @mention
+    for (var i = 0; i < messageMentions.length; i++) {
+      if (usersInMoveeer.has(messageMentions[i].id)) {
+        console.log(new Date().toLocaleTimeString() + ' - ' + message.guild.name + ' - Moving a user');
+        message.channel.send('Moving: ' + messageMentions[i] + '. By request of ' + '<@' + authorID + '>');
+        guild.member(messageMentions[i].id).setVoiceChannel(userVoiceRoomID);
+      } else {
+        if (messageMentions[i].id === authorID) {
+          // Stop people from trying to move themself
+          message.channel.send("You can't move yourself.. :) " + messageMentions[i]);
+          console.log(new Date().toLocaleTimeString() + ' - ' + message.guild.name + ' - User trying to move himself');
+          return;
         }
+        console.log(new Date().toLocaleTimeString() + ' - ' + message.guild.name + ' - User in wrong channel.');
+        message.channel.send('Not moving: ' + messageMentions[i].username + '. Is the user in the correct voice channel? (Moveer)');
       }
     }
   }
