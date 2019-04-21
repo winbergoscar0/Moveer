8dfad3706815df44dd960accf629454621859fd4
Oscar Andersson <anderssonoscar0@gmail.com>
2019-04-22 01:40:01 +0200

    Update message to fit !move aswell as !gmove

--- moveerMessage.js
+++ moveerMessage.js
@@ -10,7 +10,7 @@ const MOVEER_MISSING_MOVE_PERMISSION = "Hey! I'm not allowed to move people in t
 const MESSAGE_MISSING_ROOM_IDENTIFER = 'You need to write a number to identify a gMoveer room!'
 const GROUP_MOVE_MESSAGE_CONTAINS_MENTIONS = "You're not supposed to @mention members with this command. Try !gmove <roomNumber> instead!"
 const NO_VOICE_CHANNEL_NAMED_X = "There's no voice channel named gMoveer"
-const NO_USERS_INSIDE_ROOM = "There's no users inside gMoveer"
+const NO_USERS_INSIDE_ROOM = "There's no users inside the voice channel"
 
 module.exports = {
   USER_MOVING_SELF,
