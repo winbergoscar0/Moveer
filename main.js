c46b44f2fea206ed5ce503167918de0442a27753
Oscar Andersson <anderssonoscar0@gmail.com>
2019-04-22 17:18:05 +0200

    Update error messages and move them outside the code (#2)
    * Rework messages
    
    * Update welcome message
    
    * Make sure there are users inside Moveer when doing @mention command
    
    * Update message to fit !move aswell as !gmove
    
    * Update invite link for README.md
    
    * Fix spelling mistake
    
    * Update error messages

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ client.on('ready', () => {
 });
 
 client.on('guildCreate', (guild) => {
-  const welcomeMessage = 'Hello and thanks for inviting me! If you need help or got any questions, please head over to the official Moveer discord at https://discord.gg/8BXKe9g and ask anything!'
+  const welcomeMessage = 'Hello and thanks for inviting me! If you need help or got any questions, please head over to the official Moveer discord at https://discord.gg/8BXKe9g'
   const supportMessage = 'To get started, please follow the guide below\n 1. Create a voice channel and name it "Moveer".\n 2. Ask your friends to join "Moveer"\n 3. Join any voice channel except "Moveer"\n 4. Write `!move @friend1 @friend2`\n 5. Thats it! @friend1 & @friend2 should be moved to your voice channel'
   log.info('Joined a new server: ' + guild.name)
   let defaultChannel = "";
