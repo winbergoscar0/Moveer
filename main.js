27bc149d1522b7a6bb0fcdead04a01abcecc39ee
Oscar Andersson <anderssonoscar0@gmail.com>
2019-05-23 21:54:07 +0200

    Fix error handling when failing to send welcome message (#7)
    * Test fix for logging
    
    * Fix error handling when failing to send welcome message

--- main.js
+++ main.js
@@ -43,15 +43,14 @@ client.on('guildCreate', (guild) => {
       }
     }
   })
-
+  let message = []
+  message.push(guild)
+  if (defaultChannel === "") {
+    log.info('Failed to find defaultchannel, not sending welcome message.')
+    return;
+  }
   defaultChannel.send(welcomeMessage)
-  .catch((e) => {
-    moveerMessage.logger(message, 'Welcome Guide', e)
-    })
   defaultChannel.send(supportMessage)
-  .catch((e) => {
-    moveerMessage.logger(message, 'Welcome Support', e)
-  })
 })
 
 client.on('guildDelete', (guild) => {
