cb182824460523ca1e2df5e66ca58ec43fe3b5e8
Oscar Andersson <anderssonoscar0@gmail.com>
2019-06-01 23:11:41 +0200

    Refactorize !move

--- helper.js
+++ helper.js
@@ -1,52 +1,75 @@
-const moveerMessage = require ('./moveerMessage.js')
-
-
+const moveerMessage = require('./moveerMessage.js')
 
 function checkIfVoiceChannelExist(message, voiceChannel, channelName) {
-  if (voiceChannel === null || voiceChannel.members == undefined) {
-    throw {
-      'logMessage': 'Cant find fromVoiceChannel: ' + channelName,
-      'sendMessage': moveerMessage.NO_VOICE_CHANNEL_NAMED_X + 'the name: "' + channelName + '" <@' + message.author.id + '>'
-    }
+  if (voiceChannel === null || voiceChannel.members == undefined) throw {
+    'logMessage': 'Cant find fromVoiceChannel: ' + channelName,
+    'sendMessage': moveerMessage.NO_VOICE_CHANNEL_NAMED_X + 'the name: "' + channelName + '" <@' + message.author.id + '>'
   }
 }
 
-function checkArgsLength (args, expectedLength) {
-  if (args.length < expectedLength) {
-    throw {
-      'logMessage': 'Missing one or more arguments',
-      'sendMessage': 'Missing one or more channel names\nCommand is `!fmove fromChannel ToChannel`'
-    }
+function checkArgsLength(args, expectedLength) {
+  if (args.length < expectedLength) throw {
+    'logMessage': 'Missing one or more arguments',
+    'sendMessage': 'Missing arguments'
   }
 }
 
 function checkIfArgsTheSame(message, args) {
-  if (args[0] === args[1]) {
-    throw {
-      'logMessage': 'Same voicechannel name',
-      'sendMessage': moveerMessage.VOICE_CHANNEL_NAMES_THE_SAME + ' <@' + message.author.id + '>'
-    }
+  if (args[0] === args[1]) throw {
+    'logMessage': 'Same voicechannel name',
+    'sendMessage': moveerMessage.VOICE_CHANNEL_NAMES_THE_SAME + ' <@' + message.author.id + '>'
   }
 }
 
 function checkIfUsersInsideVoiceChannel(message, fromVoiceChannelName, fromVoiceChannel) {
   if (fromVoiceChannel === null) return
-  if (fromVoiceChannel.members.array().length < 1) {
-    throw {
-      'logMessage': 'No users inside the channel: ' + fromVoiceChannelName,
-      'sendMessage': moveerMessage.NO_USERS_INSIDE_ROOM + ':  ' + fromVoiceChannelName + ' <@' + message.author.id + '>'
-    }
+  if (fromVoiceChannel.members.array().length < 1) throw {
+    'logMessage': 'No users inside the channel: ' + fromVoiceChannelName,
+    'sendMessage': moveerMessage.NO_USERS_INSIDE_ROOM + ':  ' + fromVoiceChannelName + ' <@' + message.author.id + '>'
   }
 }
 
 
 function checkIfTextChannelIsMoveerAdmin(message) {
-  if (message.channel.name.toLowerCase() !== 'moveeradmin') {
-    throw {
-      'logMessage': 'Command made outside moveeradmin',
-      'sendMessage': moveerMessage.CMOVE_OUTSIDE_MOVEERADMIN + ' <@' + message.author.id + '>'
-    }
-  } 
+  if (message.channel.name.toLowerCase() !== 'moveeradmin') throw {
+    'logMessage': 'Command made outside moveeradmin',
+    'sendMessage': moveerMessage.CMOVE_OUTSIDE_MOVEERADMIN + ' <@' + message.author.id + '>'
+  }
+}
+
+function checkForUserMentions(message, messageMentions) {
+  if (messageMentions.length < 1) throw {
+    'logMessage': '@Mention is missing',
+    'sendMessage': moveerMessage.MESSAGE_MISSING_MENTION + ' <@' + message.author.id + '>'
+  }
+}
+
+function checkIfSelfMention(message) {
+  if (message.mentions.users.has(message.author.id)) throw {
+    'logMessage': 'User trying to move himself',
+    'sendMessage': moveerMessage.USER_MOVING_SELF + ' <@' + message.author.id + '>'
+  }
+}
+
+function checkIfAuthorInsideAVoiceChannel(message, userVoiceRoomID) {
+  if (userVoiceRoomID === undefined || userVoiceRoomID === null) throw {
+    'logMessage': 'User tried to move people without being inside a voice room',
+    'sendMessage': moveerMessage.USER_NOT_IN_ANY_VOICE_CHANNEL + ' <@' + message.author.id + '>'
+  }
+}
+
+function checkIfVoiceChannelContainsMoveer(message, authorVoiceChannelName) {
+  if (authorVoiceChannelName.toLowerCase().includes('moveer')) throw {
+    'logMessage': 'User trying to move people into a moveer channel',
+    'sendMessage': moveerMessage.USER_INSIDE_MOVEER_VOICE_CHANNEL + ' <@' + message.author.id + '>'
+  }
+}
+
+function checkIfGuildHasTwoMoveerChannels(message) {
+  if (message.guild.channels.find(channel => channel.name === 'Moveer') !== null && message.guild.channels.find(channel => channel.name === 'moveer') !== null) throw {
+    'logMessage': 'User has two channels called moveer/Moveer',
+    'sendMessage': moveerMessage.SERVER_HAS_TWO_MOVEER_VOICE_CHANNELS
+  }
 }
 
 
@@ -54,6 +77,36 @@ function checkIfTextChannelIsMoveerAdmin(message) {
 
 
 
+function checkForConnectPerms(message) {
+  if (!message.member.voiceChannel.memberPermissions(message.guild.me).has('CONNECT')) throw {
+    'logMessage': 'Moveer is missing CONNECT permission',
+    'sendMessage': moveerMessage.MOVEER_MISSING_CONNECT_PERMISSION + ' <@' + authorID + '> \n' + moveerMessage.SUPPORT_MESSAGE
+  }
+}
+
+function checkForMovePerms(message) {
+  if (!message.member.voiceChannel.memberPermissions(message.guild.me).has('CONNECT')) throw {
+    'logMessage': 'Moveer is missing Move Members permission (Missing when adding to the discord, reinvite the bot or check the room permissions)',
+    'sendMessage': moveerMessage.MOVEER_MISSING_MOVE_PERMISSION + ' <@' + authorID + '> \n' + moveerMessage.SUPPORT_MESSAGE
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+// Helper functions
+
+
+function getNameOfVoiceChannel (message, voiceChannelId) {
+  return message.guild.channels.get(voiceChannelId).name
+}
 
 
 
@@ -63,5 +116,13 @@ module.exports = {
   checkIfVoiceChannelExist,
   checkIfArgsTheSame,
   checkIfUsersInsideVoiceChannel,
-  checkArgsLength
+  checkArgsLength,
+  checkForUserMentions,
+  checkIfAuthorInsideAVoiceChannel,
+  checkForConnectPerms,
+  checkForMovePerms,
+  checkIfVoiceChannelContainsMoveer,
+  checkIfGuildHasTwoMoveerChannels,
+  getNameOfVoiceChannel,
+  checkIfSelfMention
 }
--- move.js
+++ move.js
@@ -1,123 +1,72 @@
 const moveerMessage = require('./moveerMessage.js')
+const helper = require('./helper.js')
 
-function move (args, message, command) {
-    const guild = message.guild; // The guild where the user sends its message
-    const userVoiceRoomID = message.member.voiceChannelID; // ID of the authors voice room
-    const authorID = message.author.id; // The author ID
-    const messageMentions = message.mentions.users.array(); // Mentions in the message
-    const guildChannels = guild.channels.find(channel => channel.name.toLowerCase() === 'moveer')
-    const textChannelName = message.channel.name
+function move(args, message, command) {
+  const fromVoiceChannel = message.guild.channels.find(channel => channel.name.toLowerCase() === 'moveer')
+  const fromVoiceChannelName = helper.getNameOfVoiceChannel(message, fromVoiceChannel.id)
+  const authorVoiceRoomId = message.member.voiceChannelID; // ID of the authors voice room
+  const authorVoiceChannelName = helper.getNameOfVoiceChannel(message, authorVoiceRoomId)
+  const messageMentions = message.mentions.users.array(); // Mentions in the message
 
-    // Make sure the user @mentions someone
-    if (args < 1 || messageMentions.length < 1) {
-      moveerMessage.logger(message, command, '@Mention is missing')
-      moveerMessage.sendMessage(message, (moveerMessage.MESSAGE_MISSING_MENTION + ' <@' + authorID + '>'))
-      return;
+  try {
+    helper.checkArgsLength(args, 1)
+    helper.checkForUserMentions(message, messageMentions)
+    helper.checkIfAuthorInsideAVoiceChannel(message, authorVoiceRoomId)
+    helper.checkForMovePerms(message)
+    helper.checkForConnectPerms(message)
+    helper.checkIfSelfMention(message)
+    if (message.channel.name.toLowerCase() !== 'moveeradmin') { 
+      helper.checkIfVoiceChannelExist(message, fromVoiceChannel, authorVoiceChannelName)
+      helper.checkIfVoiceChannelContainsMoveer(message, authorVoiceChannelName)
+      helper.checkIfGuildHasTwoMoveerChannels(message)
+      helper.checkIfUsersInsideVoiceChannel(message, fromVoiceChannelName, fromVoiceChannel)
     }
+  }
+  catch (err) {
+    moveerMessage.logger(message, command, err.logMessage)
+    moveerMessage.sendMessage(message, err.sendMessage)
+    return
+  }
 
-    if (message.mentions.users.has(authorID)) {
-      moveerMessage.logger(message, command, 'User trying to move himself')
-      moveerMessage.sendMessage(message, (moveerMessage.USER_MOVING_SELF + ' <@' + authorID + '>'))
-      return;
+  // No errors in the message, lets get moving!
+  let usersMoved = 0
+  if (message.channel.name.toLowerCase() === 'moveeradmin') {
+    // START - Command came from moveeradmin, don't requrire users to be inside Moveer
+    for (var i = 0; i < messageMentions.length; i++) {
+      if (message.guild.members.get(messageMentions[i].id).voiceChannelID === undefined || message.guild.members.get(messageMentions[i].id).voiceChannelID === null) {
+        moveerMessage.logger(message, command, 'Not moving user, not in any voice channel!')
+        moveerMessage.sendMessage(message, (messageMentions[i] + ' ' + moveerMessage.USER_MENTION_NOT_IN_ANY_CHANNEL))
+      } else if (message.guild.members.get(messageMentions[i].id).voiceChannelID === authorVoiceRoomId) {
+        moveerMessage.logger(message, command, 'Not moving user, user already in the channel!')
+        moveerMessage.sendMessage(message, (messageMentions[i].username + ' ' + moveerMessage.USER_ALREADY_IN_CHANNEL))
+      } else {
+        message.guild.member(messageMentions[i].id).setVoiceChannel(authorVoiceRoomId);
+        usersMoved = usersMoved + 1
+      }
     }
-
-    // Stop people from trying to move people without being inside a voice room
-    if (userVoiceRoomID === undefined || userVoiceRoomID === null) {
-      moveerMessage.logger(message, command, 'User tried to move people without being inside a voice room')
-      moveerMessage.sendMessage(message, (moveerMessage.USER_NOT_IN_ANY_VOICE_CHANNEL + ' <@' + authorID + '>'))
-      return;
+    if (usersMoved > 0) {
+      moveerMessage.logger(message, command, ('Admin moved ' + usersMoved + (usersMoved === 1 ? " user" : " users")))
+      moveerMessage.sendMessage(message, ('Moved ' + usersMoved + (usersMoved === 1 ? " user" : " users") + ' by request of ' + ' <@' + message.author.id + '>'))
     }
-    
-    if (textChannelName.toLowerCase() !== 'moveeradmin') {
-      // If the message comes from the admin room, don't require the users to be inside Moveer
-
-      const userVoiceRoomName = guild.channels.get(userVoiceRoomID).name // Name of authors voice room
-      // Stop people from trying to move people into Moveer
-      if (userVoiceRoomName.toLowerCase().includes('moveer')){
-        moveerMessage.logger(message, command, 'User trying to move people into a moveer channel')
-        moveerMessage.sendMessage(message, (moveerMessage.USER_INSIDE_MOVEER_VOICE_CHANNEL + ' <@' + authorID + '>'))
-        return;
+    // END - Command came from moveeradmin, don't require users to be inside Moveer
+  } else {
+    // START - Command not sent from moveeradmin, make sure the users are inside Moveer
+    for (var i = 0; i < messageMentions.length; i++) {
+      const usersInMoveeer = fromVoiceChannel.members; // The members ofthe Moveer voice room
+      if (!usersInMoveeer.has(messageMentions[i].id) || message.guild.members.get(messageMentions[i].id).voiceChannelID === undefined) {
+        moveerMessage.logger(message, command, 'User not inside "Moveer"')
+        moveerMessage.sendMessage(message, ('Not moving: ' + messageMentions[i].username + '. Is the user in the voice channel "Moveer"?'))
+      } else {
+        message.guild.member(messageMentions[i].id).setVoiceChannel(authorVoiceRoomId);
+        usersMoved = usersMoved + 1
       }
-
-      if (guild.channels.find(channel => channel.name === 'Moveer') !== null && guild.channels.find(channel => channel.name === 'moveer') !== null) {
-        moveerMessage.logger(message, command, 'User has two channels called moveer/Moveer')
-        moveerMessage.sendMessage(message, moveerMessage.SERVER_HAS_TWO_MOVEER_VOICE_CHANNELS)
-        return;
-      }
-      
-      // Check for errors in the message
-      // Make sure there's a voice room called Moveer
-      if (guildChannels === null || guildChannels.members == undefined) {
-        moveerMessage.logger(message, command, 'No voice channel called Moveer')
-        moveerMessage.sendMessage(message, moveerMessage.SERVER_IS_MISSING_MOVEER_VOICE_CHANNEL)
-        moveerMessage.sendMessage(message, moveerMessage.SUPPORT_MESSAGE)
-        return;
-      }
-
-      // Make sure there are users inside Moveer
-        if (guildChannels.members.length < 1) {
-          moveerMessage.logger(message, command, 'No users inside the Moveer channel')
-          moveerMessage.sendMessage(message, (moveerMessage.NO_USERS_INSIDE_ROOM + ' Moveer <@' + authorID + '>'))
-          return;
-        }
     }
-
-    // Check that moveer has access to the voice room
-    if (!message.member.voiceChannel.memberPermissions(guild.me).has('CONNECT')) {
-      moveerMessage.logger(message, command, 'Moveer is missing CONNECT permission')
-      moveerMessage.sendMessage(message, (moveerMessage.MOVEER_MISSING_CONNECT_PERMISSION + ' <@' + authorID + '>'))
-      moveerMessage.sendMessage(message, moveerMessage.SUPPORT_MESSAGE)
-      return;
+    if (usersMoved > 0) {
+      moveerMessage.logger(message, command, ('Moved ' + usersMoved + (usersMoved === 1 ? " user" : " users")))
+      moveerMessage.sendMessage(message, ('Moved ' + usersMoved + (usersMoved === 1 ? " user" : " users") + ' by request of' + ' <@' + message.author.id + '>'))
     }
-
-    // Check that moveer has move members role 
-    if (!guild.me.hasPermission('MOVE_MEMBERS')) {
-      moveerMessage.logger(message, command, 'Moveer is missing Move Members permission (Missing when adding to the discord, reinvite the bot or check the room permissions)')
-      moveerMessage.sendMessage(message, (moveerMessage.MOVEER_MISSING_MOVE_PERMISSION + ' <@' + authorID + '>'))
-      moveerMessage.sendMessage(message, moveerMessage.SUPPORT_MESSAGE)
-      return;
-    }
-  
-    let usersMoved = 0
-    // No errors in the message, try moving everyone in the @mention
-    if (textChannelName.toLowerCase() === 'moveeradmin'){
-      // START - Command came from moveeradmin, don't requrire users to be inside Moveer
-      for (var i = 0; i < messageMentions.length; i++) {
-        if (message.guild.members.get(messageMentions[i].id).voiceChannelID === undefined || message.guild.members.get(messageMentions[i].id).voiceChannelID === null) {
-          moveerMessage.logger(message, command, 'Not moving user, not in any voice channel!')
-          moveerMessage.sendMessage(message, (messageMentions[i] + ' ' + moveerMessage.USER_MENTION_NOT_IN_ANY_CHANNEL))
-        } else if (message.guild.members.get(messageMentions[i].id).voiceChannelID === userVoiceRoomID) {
-          moveerMessage.logger(message, command, 'Not moving user, user already in the channel!')
-          moveerMessage.sendMessage(message, (messageMentions[i].username + ' ' + moveerMessage.USER_ALREADY_IN_CHANNEL))
-        }  else {
-          guild.member(messageMentions[i].id).setVoiceChannel(userVoiceRoomID);
-          usersMoved = usersMoved + 1
-          
-        }
-      }
-      if (usersMoved > 0) {
-        moveerMessage.logger(message, command, ('Admin moved ' + usersMoved + (usersMoved === 1 ? " user" : " users")))
-        moveerMessage.sendMessage(message, ('Moved ' + usersMoved + (usersMoved === 1 ? " user" : " users") + ' by request of ' + ' <@' + authorID + '>'))
-      }
-      // END - Command came from moveeradmin, don't require users to be inside Moveer
-    } else {
-      // START - Command not sent from moveeradmin, make sure the users are inside Moveer
-      for (var i = 0; i < messageMentions.length; i++) {
-        const usersInMoveeer = guildChannels.members; // The members ofthe Moveer voice room
-        if (!usersInMoveeer.has(messageMentions[i].id) || message.guild.members.get(messageMentions[i].id).voiceChannelID === undefined) {
-          moveerMessage.logger(message, command, 'User not inside "Moveer"')
-          moveerMessage.sendMessage(message, ('Not moving: ' + messageMentions[i].username + '. Is the user in the voice channel "Moveer"?'))
-        } else {
-          guild.member(messageMentions[i].id).setVoiceChannel(userVoiceRoomID);
-          usersMoved = usersMoved + 1
-        }
-      }
-      if (usersMoved > 0) {
-        moveerMessage.logger(message, command, ('Moved ' + usersMoved + (usersMoved === 1 ? " user" : " users")))
-        moveerMessage.sendMessage(message, ('Moved ' + usersMoved + (usersMoved === 1 ? " user" : " users") + ' by request of' + ' <@' + authorID + '>'))
-      }
-      // END - Command not sent from moveeradmin, make sure the users are inside Moveer
-    } 
+    // END - Command not sent from moveeradmin, make sure the users are inside Moveer
+  }
 }
 
 
