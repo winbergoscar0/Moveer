bf632ad40a91fd18292ff2ad59577e6c021b1c9c
Oscar Andersson <anderssonoscar0@gmail.com>
2019-06-04 21:16:57 +0200

    Add trackinf for successful moves (#13)

--- example.config.js
+++ example.config.js
@@ -4,5 +4,6 @@ module.exports = {
   discordPrefix: '!',
   discordBotListToken: 'x', // <---- DON'T CHANGE THIS X
   discordHookToken: 'x', // <---- DON'T CHANGE THIS X
-  discordHookIdentifier: 'x' // <---- DON'T CHANGE THIS X
+  discordHookIdentifier: 'x', // <---- DON'T CHANGE THIS X
+  postgreSQLConnection: 'x' // <---- DON'T CHANGE THIS X
 };
