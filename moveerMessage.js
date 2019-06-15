d861d8e27a44741bcd57100bbff033941f5699f0
Oscar Andersson <anderssonoscar0@gmail.com>
2019-06-15 23:16:59 +0200

    Log error when DBL token & got send message

--- moveerMessage.js
+++ moveerMessage.js
@@ -117,9 +117,11 @@ function sendMessage(message, sendMessage) {
   const helper = require('./helper.js')
   message.channel.send(sendMessage)
     .catch((e) => {
-      console.log(e)
       logger(message, message.content, e)
-      if (config.discordBotListToken !== 'x' && message.channel.permissionsFor(message.guild.me).has("SEND_MESSAGES") === true) helper.reportMoveerError(message)
+      if (config.discordBotListToken !== 'x' && message.channel.permissionsFor(message.guild.me).has("SEND_MESSAGES") === true) {
+        helper.reportMoveerError(message)
+        console.log(e)
+      }
     });
 }
 
