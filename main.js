25e10478f25a83d5765ed0cff9d51d29842c7791
Oscar Andersson <oscar.andersson@bergendahls.se>
2019-07-02 09:03:26 +0200

    Guilds

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ client.on('guildDelete', (guild) => {
 // Listen for messages
 client.on('message', message => {
   if (!message.content.startsWith(config.discordPrefix)) return
-  if (message.author.bot) return
+  if (message.author.bot && config.allowedGuilds.indexOf(message.guild.id) === -1) return
   if (message.channel.type !== 'text') return
   const args = message.content.slice(config.discordPrefix.length).trim().split(/ +/g)
   const command = args.shift().toLowerCase()
