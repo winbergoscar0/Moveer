1e1a25ab1d478fc4ecf63fed1b33435807b188f2
Oscar Andersson <anderssonoscar0@gmail.com>
2019-09-25 18:34:17 +0200

    Update example config

--- example.config.js
+++ example.config.js
@@ -5,5 +5,7 @@ module.exports = {
   discordBotListToken: 'x', // <---- DON'T CHANGE THIS X
   discordHookToken: 'x', // <---- DON'T CHANGE THIS X
   discordHookIdentifier: 'x', // <---- DON'T CHANGE THIS X
-  postgreSQLConnection: 'x' // <---- DON'T CHANGE THIS X
+  postgreSQLConnection: 'x', // <---- DON'T CHANGE THIS X
+  allowedGuilds: [], // <---- DON'T CHANGE THIS
+  blockedVoiceChannels: [] // <---- DON'T CHANGE THIS
 }
