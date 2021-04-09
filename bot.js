4a34b694cd50a59e11a80634029223fc239e8474
Oscar Andersson <anderssonoscar0@gmail.com>
2021-04-09 14:30:15 +0000

    Auto spawn shards instead of hardcoded 5 limit

--- bot.js
+++ bot.js
@@ -3,6 +3,6 @@ const config = require('./config.js')
 
 const manager = new ShardingManager('./main.js', { token: config.discordToken, autoSpawn: true })
 
-manager.spawn(5)
+manager.spawn()
 manager.on('launch', (shard) => console.log(`[SHARD-LAUNCH] Shard ${shard.id}/${shard.manager.totalShards}`))
 manager.on('shardCreate', (shard) => console.log(`[SHARD-LAUNCH] Shard ${shard.id}/${shard.manager.totalShards}`))
